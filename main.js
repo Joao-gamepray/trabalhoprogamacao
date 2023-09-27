e0e0393dd1ed34cfdf7487d01ce5771628e6570f
Joao-gamepray <110043744+Joao-gamepray@users.noreply.github.com>
2023-09-27 09:05:50 -0300

    prog
    a

--- main.js
+++ main.js
@@ -1,38 +1,4 @@
-function tocaSomPom(){
-    document.querySelector('som_tecla_som').play();
+function tocaSomPom(){ 
+    document.querySelector('som_tecla_som').play(); 
     }
-
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
-
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-
-}
+    
